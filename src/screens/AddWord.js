4667a307dddf7a12d09ac63ecb190dbdc1461d0d
Tolu Annie <toluannie@gmail.com>
2022-11-17 12:00:44 +0100

    dependencies added, new screens created

---
+++ src/screens/AddWord.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function AddWord() {
+    return (
+      <View>
+        <Text>Talent Plus</Text>
+      </View>
+    );  
+}
